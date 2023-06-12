c9a6a638e8922b800eafa4f534fd3a3fbfa692c2
ohhanadavid <sustujbv167@gmail.com>
2023-06-12 22:28:33 +0300

    end part b

--- demo/src/main/java/sortingClean/AlgorithmRunner.java
+++ demo/src/main/java/sortingClean/AlgorithmRunner.java
@@ -1,13 +1,21 @@
 package sortingClean;
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.util.Random;
 
 // TODO: Add dependency injection and annotations to this file
 public class AlgorithmRunner {
-    SortingAlgorithm<Integer> quadraticAlgorithm = new BubbleSort();
-    SortingAlgorithm<Integer> nlognAlgorithm = new QuickSort();
-    SortingAlgorithm<Integer> randomAlgorithm1 = makeRandomSortingAlgorithm();
-    SortingAlgorithm<Integer> randomAlgorithm2 = makeRandomSortingAlgorithm();
-    int numberOfElements = 10000;
+    @Inject
+    @Named("ABubbleSort") SortingAlgorithm<Integer> quadraticAlgorithm ;
+    @Inject
+    @Named("AQuickSort") SortingAlgorithm<Integer> nlognAlgorithm ;
+    @Inject
+    @Named("RandomSortingAlgorithm") SortingAlgorithm<Integer> randomAlgorithm1 ;
+    @Inject
+    @Named("RandomSortingAlgorithm") SortingAlgorithm<Integer> randomAlgorithm2 ;
+
+    @Inject
+    @Sweet int numberOfElements ;
     public void runAlgorithms(){
         Random rand = new Random();
         Integer[] ints = rand.ints(1,Integer.MAX_VALUE)
@@ -25,18 +33,5 @@ public class AlgorithmRunner {
         randomAlgorithm2.sort(intsClone);
     }
 
-    private static SortingAlgorithm<Integer> makeRandomSortingAlgorithm(){
-        Random random = new Random(System.currentTimeMillis());
-        SortingAlgorithm<Integer> sortAlg= null;
-        switch (random.nextInt(4)){
-            case 0: sortAlg = new QuickSort();
-                break;
-            case 1: sortAlg = new MergeSort();
-                break;
-            case 2: sortAlg = new BubbleSort();
-                break;
-            case 3: sortAlg = new InsertionSort();
-        }
-        return sortAlg;
-    }
+
 }
--- demo/src/main/java/sortingClean/MainApp.java
+++ demo/src/main/java/sortingClean/MainApp.java
@@ -1,12 +1,53 @@
 package sortingClean;
 
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.Random;
+
 // TODO: Add java classes (in separate files for annotations and aspects)
 public class MainApp {
+    static WeldContainer container = new Weld().initialize();
+
     public static void main(String[] args) {
         // TODO: Change this line to initialize an injected AlgorithmRunner
-        AlgorithmRunner algorithmRunner = new AlgorithmRunner();
+        AlgorithmRunner algorithmRunner = container.select(AlgorithmRunner.class).get();
 
         algorithmRunner.runAlgorithms();
     }
     // TODO: Add producers
+    private static SortingAlgorithm<Integer> makeRandomSortingAlgorithm(){
+        Random random = new Random(System.currentTimeMillis());
+        SortingAlgorithm<Integer> sortAlg= null;
+        switch (random.nextInt(4)){
+            case 0: sortAlg = container.select(QuickSort.class).get();
+                break;
+            case 1: sortAlg = container.select(MergeSort.class).get();
+                break;
+            case 2: sortAlg =container.select(BubbleSort.class).get();
+                break;
+            case 3: sortAlg = container.select(InsertionSort.class).get();
+        }
+        return sortAlg;
+    }
+
+    @Produces
+    public @Named("AQuickSort") SortingAlgorithm<Integer> quickSort()
+    {    return container.select(QuickSort.class).get();}
+    @Produces
+    public @Named("ABubbleSort") SortingAlgorithm<Integer> bubbleSort()
+    {    return container.select(BubbleSort.class).get();}
+    @Produces
+    public @Named("RandomSortingAlgorithm") SortingAlgorithm<Integer> randomSortingAlgorithm()
+    {    return makeRandomSortingAlgorithm();}
+
+    @Produces
+    public @Sweet int getNumOfSpoonsSweet()
+    {
+        return 10000;
+    }
+
+
 }
